72ce6c3a5f5ee5ee7a416250ecaf3537471b3476
Ricardo Oliveira <ricardo@hashicorp.com>
2022-08-03 11:28:58 +0100

    Update main.tf

--- tfc-demo-gcp/main.tf
+++ tfc-demo-gcp/main.tf
@@ -33,7 +33,7 @@ resource "google_compute_instance" "vm_instance" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "debian-cloud/debian-11"
     }
   }
 
